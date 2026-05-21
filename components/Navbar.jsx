48649c86824d10bc44d84d66fe6d9041af374195
PhantomOblivion <smasif404@gmail.com>
2026-05-22 01:15:44 +0600

    build dynamic navigation bar with login/logout functionality.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,6 +42,8 @@ export default function Navbar() {
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
+
+                        {/* This is Logo section */}
           <Link href="/" className="flex items-center gap-2">
             <div className="rounded-full flex items-center justify-center shrink-0">
               <span className="">
